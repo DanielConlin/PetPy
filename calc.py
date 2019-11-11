3f1f1afa39a6134c38ba1541bda0ab1867a8dab1
Daniel.Conlin <Daniel.Conlin@shell.com>
2019-11-11 15:57:41 -0600

    Add densidty calculation

--- calc.py
+++ calc.py
@@ -3,3 +3,8 @@ import nupmy as np
 def acoustic_impedance(rho, vp):
     """Calculate acoustic impedance of a rock"""
     return rho * vp
+
+def density(mass, volume):
+    """Calculate density"""
+    return mass / volume
+    
